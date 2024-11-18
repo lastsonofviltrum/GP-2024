baf8a0e1b33b7523358f2c70d152a16d6849609b
lastsonofviltrum <charlieryan1515@gmail.com>
2024-11-18 14:26:56 +0000

    November 18th, 2024, 2:26 PM

---
+++ shooter71024/shooter-71024/november_18th_2024.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var i = 0
+
+
+func _draw() -> void:
+	i += 1
+	print ("draw called: " + str(i))
+	var r = get_viewport_rect()
+	r.size.x = r.size.x / 2
+	draw_rect(Rect2(r), Color.RED, true, 10, true)
+	
+var p = get_viewport().get_mouse_position()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
