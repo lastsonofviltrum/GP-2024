8718d24cc8b6866bfc6bdc1c6c5a9230dc32b40a
lastsonofviltrum <charlieryan1515@gmail.com>
2025-01-30 15:40:59 +0000

    January 30th 2025, 3:40 PM

--- jan-30-2025/loops.gd
+++ jan-30-2025/loops.gd
@@ -9,13 +9,21 @@ func _process(delta: float) -> void:
 func _draw() -> void:
 	var r = Rect2(10, 10, 100, 30)
 	draw_rect(r, Color.MEDIUM_SEA_GREEN)
-	
 	draw_line(Vector2(50, 50), Vector2(200, 200), Color.BROWN)
-	
 	draw_circle(Vector2(500, 500), 100, Color.FUCHSIA, true)
+	
+	var v = get_viewport_rect()
+	
+	draw_line(Vector2(0, v.size.y), Vector2(v.size.x, 0), Color.AQUAMARINE)
+	
+	for i in range(10):
+		print(i)
+	
 	pass
 	
 	var basket = ["apple", "banana", "carrot"]
 	
 	for i in range(basket.size()):
 		print(basket[i])
+	
+	
