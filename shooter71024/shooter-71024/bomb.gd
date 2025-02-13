6981f3daf960b1f619e3fb4febe3212a5b8ae092
lastsonofviltrum <charlieryan1515@gmail.com>
2025-02-13 12:21:23 +0000

    Update bomb.gd

--- shooter71024/shooter-71024/bomb.gd
+++ shooter71024/shooter-71024/bomb.gd
@@ -3,6 +3,7 @@ var speed = 300
 
 func  _physics_process(delta):
 	position.y += speed * delta
+<<<<<<< HEAD:shooter71024/shooter-71024/bomb.gd
 
 	
 
@@ -11,3 +12,5 @@ func _on_area_entered(area):
 	print(area)
 	
 	pass # Replace with function body.
+=======
+>>>>>>> upstream/main:the-course/bomb.gd
