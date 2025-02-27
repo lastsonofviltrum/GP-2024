4f65654e9355f64da9127f1bda5ee7f5e34ec571
lastsonofviltrum <charlieryan1515@gmail.com>
2025-02-27 15:23:51 +0000

    February 27th, 2025, 3:23 PM

--- the-course/dude_enemy.gd
+++ the-course/dude_enemy.gd
@@ -1,1 +1,18 @@
 extends CharacterBody2D
+
+@export var speed = 200
+
+var dude
+
+func _ready() -> void:
+	dude = $"../../dude"
+	
+func _physics_process(delta:float) -> void:
+	var to_player:Vector2 = dude.position - position
+	to_player = to_player.normalized()
+	velocity + to_player * speed
+	move_and_slide()
+	
+	func _on_area_2d_body_exited(body: Node2D)
+		if body == dude:
+			state = State.IDLE
--- the-course/ufo_spawner.gd
+++ the-course/ufo_spawner.gd
@@ -29,12 +29,5 @@ var velocity = 400
 var dude
 
 func _ready() -> void:
-	dude = $"../../dude"
-	
-func _physics_process(delta:float) -> void:
-	var to_player:Vector2 = dude.position - position
-	to_player = to_player.normalized() 
-	velocity + to_player * speed
-	
-	
+
 pass
