f4238980bf4247d77274f1d9353097c44ed79278
lastsonofviltrum <charlieryan1515@gmail.com>
2025-02-27 12:51:09 +0000

    February 27th, 2025, 12:51 PM

---
+++ feb27th2025/coin.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+func _process(delta: float) -> void:
+	pass
+	
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		body.score = body.score + 1
+		self.queue_free
+	pass
